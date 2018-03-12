c066ecdae9ce449056abe1790c74894a36f50f73
IulianIonescu <iulius_stefi@yahoo.com>
2018-03-12 08:53:48 +0200

    Add wait and notify for threads

--- PDA/ProdCons_SemWithMutex/Semaphore_simulated.java
+++ PDA/ProdCons_SemWithMutex/Semaphore_simulated.java
@@ -2,39 +2,53 @@ public class Semaphore_simulated {
 
 	private Integer number_of_permits;
 	private String name;
+	private final int MAX ;
 
 	
 	public Semaphore_simulated(int number_of_permits,String name) {
+		super();
+		MAX = number_of_permits;
 		this.number_of_permits = number_of_permits;
 		this.name = name;
 		
 	}
 	
-	public void acquire() {
-			
-		boolean semaphoreNotAquired = true;
-		
-		while (semaphoreNotAquired){ 
-			synchronized(number_of_permits) {
+	public void acquire() {	
+			synchronized(this) {
 			
 				if(number_of_permits > 0) {
 					//Down 
 					number_of_permits -- ;
-					semaphoreNotAquired = false;
+					
 					System.out.printf("Semaphore %s Taken %n",name);
 				}
+				if(number_of_permits == 0)
+				{
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
-		}
+		
 
 	}
 	
 	public void release() {
 		
-		synchronized(number_of_permits) {
+		synchronized(this) {
+			if(number_of_permits == 0) {
+				
+				//Up
+				number_of_permits ++;
+				notify();
+			}
 			
-			//Up
-			number_of_permits ++;
-			
+			if(number_of_permits < MAX - 2 && number_of_permits > 1) {
+				number_of_permits ++;
+			}
 			System.out.printf("Semaphore %s Released %n",name);
 		}
 	}
